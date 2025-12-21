b04217d3844c42749c3d7ff4858bca8285e529f9
Ahad <ahadsaim9@gmail.com>
2025-12-21 22:19:54 +0600

    moved all component root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
-import { HomePage } from "@/components/HomePage";
-import React from "react";
+"use client";
 
-const Page = () => {
+import CallSchedule from "@/app/_partials/CallSchedule";
+import ClientReviews from "@/app/_partials/ClientReviews";
+import FeaturedWorks from "@/app/_partials/FeaturedWorks";
+import HeroSection from "@/app/_partials/HeroSection";
+import LatestfromOurBlog from "@/app/_partials/LatestfromOurBlog";
+import OurIndustryExpertise from "@/app/_partials/OurIndustryExpertise";
+import OurPartnerships from "@/app/_partials/OurPartnerships";
+import TheTechnologyWeUse from "@/app/_partials/TheTechnologyWeUse";
+import WhatWeAreSection from "@/app/_partials/WhatWeAreSection";
+import WhatWeDoSection from "@/app/_partials/WhatWeDoSection";
+
+const HomePage = () => {
   return (
-    <div>
-      <HomePage />
+    <div className="min-h-screen">
+      <HeroSection />
+      <OurPartnerships />
+      <WhatWeAreSection />
+      <WhatWeDoSection />
+      <OurIndustryExpertise />
+      <TheTechnologyWeUse />
+      <FeaturedWorks />
+      <ClientReviews />
+      <LatestfromOurBlog />
+      <CallSchedule />
     </div>
   );
 };
-
-export default Page;
+export default HomePage;
