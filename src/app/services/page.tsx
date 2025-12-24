d7e1edff5a6b3a9d85994a3c68885fdb0e9f92a8
Ahad <ahadsaim9@gmail.com>
2025-12-24 16:32:33 +0600

    remove service page use client and more changes

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { Button } from "@mui/material";
-
 import CaseStudies from "./_partials/CaseStudies";
 import OurServices from "./_partials/OurServices";
 import LetUsBuild from "./_partials/LetUsBuild";
@@ -9,13 +5,8 @@ import LetUsBuild from "./_partials/LetUsBuild";
 const ServicesPage = () => {
   return (
     <div className="min-h-screen">
-      {/* Hero Section */}
       <OurServices />
-
-      {/* Case Studies */}
       <CaseStudies />
-
-      {/* CTA Section */}
       <LetUsBuild />
     </div>
   );
