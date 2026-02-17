9ac337b0a1b707555c8621348247724435fb061b
Ahad <ahadsaim9@gmail.com>
2026-02-17 11:25:55 +0600

    added FAQ for servives page

---
+++ src/app/services/_partials/FAQSection.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { ArrowRight } from "@mui/icons-material";
+import ZtsButton from "@/app/_components/ZtButton";
+import ServicesFAQ from "../_components/ServicesFAQ";
+
+const FAQSection = () => {
+  return (
+    <section className="bg-white py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-8 text-center">
+          Frequently Asked Questions
+        </h2>
+        <section className="grid grid-cols-12 gap-4 py-8">
+          <div className="col-span-12 md:col-span-4  mb-5 flex flex-col justify-center w-full items-center md:items-start md:justify-start">
+            <h3 className="text-2xl md:text-3xl font-semibold md:font-bold text-gray-900 mb-5">
+              <span>
+                Got Questions<span className="text-primary">?</span> <br />
+                We've Got Answers
+              </span>
+            </h3>
+            <Link href="/schedule">
+              <ZtsButton
+                icon={
+                  <>
+                    <ArrowRight className="w-4 h-4" />
+                  </>
+                }
+                iconPlacement="after"
+                text="Contact Us"
+                className="border-none text-white bg-primary hover:border-transparent hover:bg-primary-dark hover:text-white"
+              />
+            </Link>
+          </div>
+          <div className="space-y-4 col-span-12  md:col-span-8">
+            <ServicesFAQ />
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default FAQSection;
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,7 @@
 import CaseStudies from "../_partials/CaseStudies";
 import OurIndustryExpertise from "../_partials/OurIndustryExpertise";
 import OurPartnerships from "../_partials/OurPartnerships";
+import FAQSection from "./_partials/FAQSection";
 import LetUsBuild from "./_partials/LetUsBuild";
 import OurServices from "./_partials/OurServices";
 import ServiceCtaSection from "./_partials/ServiceCtaSection";
@@ -15,6 +16,7 @@ const ServicesPage = () => {
       <OurIndustryExpertise />
       <OurPartnerships />
       <CaseStudies />
+      <FAQSection />
       <LetUsBuild />
     </div>
   );
