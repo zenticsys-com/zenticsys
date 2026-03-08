ffdfb9260976bea5774943b784bac7e58e292c1a
Ahad <ahadsaim9@gmail.com>
2026-03-08 11:04:25 +0600

    added accordion items

--- src/app/services/custom-website-design/_partials/CustomWebAccordion.tsx
+++ src/app/services/custom-website-design/_partials/CustomWebAccordion.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import CustomWebAccordionItems from "./CustomWebAccordionItems";
 
 const CustomWebAccordion = () => {
   return (
@@ -6,9 +7,7 @@ const CustomWebAccordion = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <section className="text-center">
           <h1 className="text-2xl  md:text-4xl font-bold text-gray-900 mb-6">
-            Building Reliable
-            <span className="text-primary"> Custom </span>
-            Software Solution
+            Custom Software Solution
           </h1>
 
           <p className="text-gray-600 mb-8 max-w-5xl mx-auto">
@@ -20,20 +19,12 @@ const CustomWebAccordion = () => {
         </section>
         <section className="grid grid-cols-12 gap-3 md:gap-5">
           <div className="col-span-12 md:col-span-7">
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Architecto autem alias facilis perferendis aperiam dignissimos
-              nostrum, eveniet odit at deserunt quae, iure quisquam. Officiis ex
-              porro, magni consectetur laudantium fugit ipsam et similique nam
-              excepturi ipsa velit deleniti, quo eius, ab quidem eligendi sed.
-              Non natus repudiandae fuga veniam totam, deleniti asperiores nisi
-              distinctio nulla!
-            </p>
+            <CustomWebAccordionItems />
           </div>
           <section className="col-span-12 md:col-span-5">
             <div className="w-full h-auto">
               <Image
-                src={"/assets/images/photo_1.png"}
+                src={"/assets/images/img_11.jpg"}
                 alt="image"
                 width={570}
                 height={380}
--- src/app/services/custom-website-design/_partials/CustomWebServices.tsx
+++ src/app/services/custom-website-design/_partials/CustomWebServices.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const CustomWebServices = () => {
   return (
-    <section className=" from-gray-200 py-12 ">
+    <section className="bg-gray-50 py-12 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl md:text-4xl  font-bold text-gray-900 mb-6">
-            Custom <span className="text-primary">Web </span> Services
+            Custom Web Services
           </h2>
           <p className="md:text-xl text-gray-600 max-w-5xl mx-auto mb-8">
             Specialized software solutions across diverse industries. We
