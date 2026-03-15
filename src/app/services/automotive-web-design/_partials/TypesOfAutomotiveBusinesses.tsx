891625fb35d3e0509b68a4499e3bc73b09f2ba67
Ahad <ahadsaim9@gmail.com>
2026-03-15 21:30:18 +0600

    comment slider

--- src/app/services/automotive-web-design/_partials/TypesOfAutomotiveBusinesses.tsx
+++ src/app/services/automotive-web-design/_partials/TypesOfAutomotiveBusinesses.tsx
@@ -3,7 +3,7 @@ import AutomotiveSlider from "../_components/AutomotiveSlider";
 const TypesOfAutomotiveBusinesses = () => {
   return (
     <div className="w-full h-screen flex  bg-blue-400 justify-center items-center">
-      <AutomotiveSlider />
+      {/* <AutomotiveSlider /> */}
     </div>
   );
 };
