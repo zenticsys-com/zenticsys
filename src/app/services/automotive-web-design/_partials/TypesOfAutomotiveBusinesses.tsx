ad1b7b338bf5509725cc4f8825b3d1576d706c14
Ahad <ahadsaim9@gmail.com>
2026-03-15 21:12:59 +0600

    added slider

--- src/app/services/automotive-web-design/_partials/TypesOfAutomotiveBusinesses.tsx
+++ src/app/services/automotive-web-design/_partials/TypesOfAutomotiveBusinesses.tsx
@@ -1,7 +1,9 @@
+import AutomotiveSlider from "../_components/AutomotiveSlider";
+
 const TypesOfAutomotiveBusinesses = () => {
   return (
     <div className="w-full h-screen flex  bg-blue-400 justify-center items-center">
-      Types Of Automotive Businesses section
+      <AutomotiveSlider />
     </div>
   );
 };
