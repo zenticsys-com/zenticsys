6f7adb8205dae7af402dabd6140468bf414aaed3
Ahad <ahadsaim9@gmail.com>
2026-03-29 14:40:10 +0600

    added slider butoon

--- src/app/services/automotive-web-design/_components/AutomotiveSlider.tsx
+++ src/app/services/automotive-web-design/_components/AutomotiveSlider.tsx
@@ -3,6 +3,8 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useRef } from "react";
+import { FaCircleArrowLeft, FaCircleArrowRight } from "react-icons/fa6";
 
 const AutomotiveSlider = () => {
   const processData = [
@@ -42,12 +44,7 @@ const AutomotiveSlider = () => {
       description:
         "Track marketing results and optimize campaigns continuously.",
     },
-    {
-      number: "06",
-      title: "Performance Tracking",
-      description:
-        "Track marketing results and optimize campaigns continuously.",
-    },
+
     {
       number: "07",
       title: "Performance Tracking",
@@ -61,10 +58,20 @@ const AutomotiveSlider = () => {
         "Track marketing results and optimize campaigns continuously.",
     },
   ];
+  const sliderRef = useRef<Slider | null>(null);
+  const handlePrevious = () => {
+    sliderRef.current?.slickPrev();
+  };
+  const handleNext = () => {
+    sliderRef.current?.slickNext();
+  };
   const settings = {
     dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
     infinite: true,
-    arrows: true,
+    arrows: false,
+    rtl: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -92,9 +99,9 @@ const AutomotiveSlider = () => {
   };
 
   return (
-    <div className="py-12">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <Slider {...settings}>
+    <div className="pt-12 pb-6">
+      <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <Slider ref={sliderRef} {...settings} focusOnSelect={true}>
           {processData.map((item, index) => (
             <div key={index} className="p-2">
               <div className="bg-white p-6 shadow-md h-full min-h-80">
@@ -107,6 +114,21 @@ const AutomotiveSlider = () => {
             </div>
           ))}
         </Slider>
+        <div className="flex gap-3 mt-2 justify-center">
+          <button
+            onClick={() => handlePrevious()}
+            className="bg-primary transition duration-300 hover:bg-green-600 font-semibold text-white  cursor-pointer rounded-full"
+          >
+            <FaCircleArrowLeft className="text-3xl p-1" />
+          </button>
+
+          <button
+            onClick={() => handleNext()}
+            className="bg-primary transition duration-300 hover:bg-green-600 font-semibold text-white  cursor-pointer rounded-full"
+          >
+            <FaCircleArrowRight className="text-3xl p-1" />
+          </button>
+        </div>
       </div>
     </div>
   );
