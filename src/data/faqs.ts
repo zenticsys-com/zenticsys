52864dfa48fb957c0ed2d43f97ec062eb49703e8
Ahad <ahadsaim9@gmail.com>
2026-04-05 15:49:05 +0600

    update Automotive Web Design FAQ page FAQ functionality

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -240,3 +240,102 @@ export const customWebAccordionItemsFaqs: FAQType[] = [
       "We don’t rush projects or over promise. We take time to understand your business, define the right structure, and build software that actually works in day-to-day operations. Our focus is clarity, clean implementation, and systems that remain stable as your company grows. No unnecessary complexity, no shortcuts, just solid engineering.",
   },
 ];
+
+/**_Automotive Accordion Items Data_**/
+export const automotiveAccordionItemsFaqs: FAQType[] = [
+  {
+    question: "Custom Automotive Website Design",
+    answer:
+      "You can contact us through our website or schedule a consultation call. After understanding your requirements, we prepare a detailed proposal outlining the project scope, timeline, and cost estimation, ensuring everything is clearly defined before we move forward.",
+  },
+  {
+    question: "Vehicle Listing System",
+    answer:
+      "We provide custom software development, SaaS product development, website design and development, mobile app development, website redesign, UI/UX design, and enterprise software solutions",
+  },
+  {
+    question: "Advanced Vehicle Search & Filter",
+    answer:
+      "Our process starts with understanding your business goals, defining clear requirements and planning, UI/UX design, system architecture setup, backend and frontend development, testing and QA, deployment, and ongoing optimization. We follow agile principles for transparency and flexibility",
+  },
+  {
+    question: "Vehicle Details Page",
+    answer:
+      "Yes. We offer post-launch support, performance optimization, security updates, and feature enhancements. After successful project delivery, we provide 6 months of complimentary support to ensure system stability and smooth operation. Following that period, we offer affordable maintenance plans for continued reliability and long-term performance.",
+  },
+  {
+    question: "Inquiry / Test Drive Booking System",
+    answer:
+      "Project timelines depend on complexity and scope. MVP projects typically take 6–10 weeks, while full-scale enterprise systems may require 12–20 weeks or more.",
+  },
+  {
+    question: "Car Inventory Management Panel",
+    answer:
+      "We don’t rush projects or over promise. We take time to understand your business, define the right structure, and build software that actually works in day-to-day operations. Our focus is clarity, clean implementation, and systems that remain stable as your company grows. No unnecessary complexity, no shortcuts, just solid engineering.",
+  },
+  {
+    question: "Mobile Friendly Responsive Design",
+    answer:
+      "We don’t rush projects or over promise. We take time to understand your business, define the right structure, and build software that actually works in day-to-day operations. Our focus is clarity, clean implementation, and systems that remain stable as your company grows. No unnecessary complexity, no shortcuts, just solid engineering.",
+  },
+  {
+    question: "SEO Optimized Structure",
+    answer:
+      "We don’t rush projects or over promise. We take time to understand your business, define the right structure, and build software that actually works in day-to-day operations. Our focus is clarity, clean implementation, and systems that remain stable as your company grows. No unnecessary complexity, no shortcuts, just solid engineering.",
+  },
+  {
+    question: "Google Map & Location Integration",
+    answer:
+      "We don’t rush projects or over promise. We take time to understand your business, define the right structure, and build software that actually works in day-to-day operations. Our focus is clarity, clean implementation, and systems that remain stable as your company grows. No unnecessary complexity, no shortcuts, just solid engineering.",
+  },
+  {
+    question: "WhatsApp / Instant Contact Integration",
+    answer:
+      "We don’t rush projects or over promise. We take time to understand your business, define the right structure, and build software that actually works in day-to-day operations. Our focus is clarity, clean implementation, and systems that remain stable as your company grows. No unnecessary complexity, no shortcuts, just solid engineering.",
+  },
+  {
+    question: "Website Speed Optimized",
+    answer:
+      "We don’t rush projects or over promise. We take time to understand your business, define the right structure, and build software that actually works in day-to-day operations. Our focus is clarity, clean implementation, and systems that remain stable as your company grows. No unnecessary complexity, no shortcuts, just solid engineering.",
+  },
+];
+
+/**_ Automotive Web Design Data_**/
+export const automotiveWebDesignFaqs: FAQType[] = [
+  {
+    question: "How can I get a project quote?",
+    answer:
+      "You can contact us through our website or schedule a consultation call. After understanding your requirements, we prepare a detailed proposal outlining the project scope, timeline, and cost estimation, ensuring everything is clearly defined before we move forward.",
+  },
+  {
+    question: "What services does Zenticsys provide?",
+    answer:
+      "We provide custom software development, SaaS product development, website design and development, mobile app development, website redesign, UI/UX design, and enterprise software solutions",
+  },
+  {
+    question: "What is your software development process?",
+    answer:
+      "Our process starts with understanding your business goals, defining clear requirements and planning, UI/UX design, system architecture setup, backend and frontend development, testing and QA, deployment, and ongoing optimization. We follow agile principles for transparency and flexibility",
+  },
+  {
+    question: "Do you provide ongoing support and maintenance?",
+    answer:
+      "Yes. We offer post-launch support, performance optimization, security updates, and feature enhancements. After successful project delivery, we provide 6 months of complimentary support to ensure system stability and smooth operation. Following that period, we offer affordable maintenance plans for continued reliability and long-term performance.",
+  },
+  {
+    question: "How long does it take to build a custom software solution?",
+    answer:
+      "Project timelines depend on complexity and scope. MVP projects typically take 6–10 weeks, while full-scale enterprise systems may require 12–20 weeks or more.",
+  },
+  {
+    question: "Why is Zenticsys different from typical development companies?",
+    answer:
+      "We don’t rush projects or overpromise. We take time to understand your business, define the right structure, and build software that actually works in day-to-day operations. Our focus is clarity, clean implementation, and systems that remain stable as your company grows. No unnecessary complexity, no shortcuts, just solid engineering.",
+  },
+
+  {
+    question: "Why should I choose Zenticsys for custom software development?",
+    answer:
+      "Zenticsys focuses on architecture-first engineering, high-performance system design, and clean code standards. We build custom software solutions aligned with long-term business growth, ensuring performance, security, and maintainability from day one.",
+  },
+];
