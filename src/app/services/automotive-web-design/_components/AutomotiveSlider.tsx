5d75ccb82aac04af4eeb1553954b84f6a1908f29
Ahad <ahadsaim9@gmail.com>
2026-04-05 10:51:45 +0600

    move to slick slider css

--- src/app/services/automotive-web-design/_components/AutomotiveSlider.tsx
+++ src/app/services/automotive-web-design/_components/AutomotiveSlider.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { useRef } from "react";
 import { FaCircleArrowLeft, FaCircleArrowRight } from "react-icons/fa6";
 
